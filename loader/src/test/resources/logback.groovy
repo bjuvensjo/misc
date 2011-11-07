04b815e9ca0e8f920b74f411c6aa8fb2ba8cab9f
Magnus Bjuvensjo <bjuvensjo@gmail.com>
2011-11-07 15:55:51 +0100

    Some updates

---
+++ loader/src/test/resources/logback.groovy
@@ -0,0 +1,15 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+
+import static ch.qos.logback.classic.Level.DEBUG
+import static ch.qos.logback.classic.Level.ERROR
+import static ch.qos.logback.classic.Level.INFO
+
+scan("30 seconds")
+
+appender("STDOUT", ConsoleAppender) {
+  encoder(PatternLayoutEncoder) {
+    pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
+  }
+}
+root(INFO, ["STDOUT"])
