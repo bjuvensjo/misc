078c787093c764c7ae656e608a7f08401cf4a007
bjuvensjo <bjuvensjo@gmail.com>
2012-06-02 17:50:04 +0200

    v1

--- js/pingpong/src/main/webapp/scripts/main.js
+++ js/pingpong/src/main/webapp/scripts/main.js
@@ -1,5 +1,5 @@
 require([ "jquery", "court", "racket", "ball", "vector2d"], function($, court, racket, ball, vector2d) {
-    var canvas = $('#mycanvas')[0], ctx = canvas.getContext('2d'), width = canvas.width, height = canvas.height,
+    var canvas = $('#game')[0], ctx = canvas.getContext('2d'), width = canvas.width, height = canvas.height,
     theCourt = court({
         ctx : ctx,
         x : 0,
@@ -18,62 +18,90 @@ require([ "jquery", "court", "racket", "ball", "vector2d"], function($, court, r
         vector2d : vector2d(30, 0),
         color : 'white'
     }), 
-    theBall = ball({
-        ctx : ctx,
-        x : width / 2,
-        y : 20,
-        r : 10,
-        vector2d : vector2d(3 - Math.floor((Math.random() * 7)), 10),
-        color : 'white'
-    });
+    theBall,
+    currentScore = 0,
+    highScore = 0,
+    currentScoreElement = $('#currentScore'),
+    highScoreElement = $('#highScore');    
     
     var draw = function() {
         ctx.clearRect(0, 0, width, height);
     	theCourt.draw();
     	theRacket.draw();
-    	theBall.draw();
+    	if (theBall) {
+    		theBall.draw();
+    	}
 	}
 
-    var lastKeyCode = 39;
+    var lastRacketKeyCode = 39;
     $(document).keydown(function(e) {
     	var keyCode = e.keyCode;
     	if (keyCode === 37 || keyCode === 39) {
-        	if (lastKeyCode !== keyCode) {
+    		// left and right arrow
+        	if (lastRacketKeyCode !== keyCode) {
         		theRacket.getVector2d().negate();
         	}    
-        	lastKeyCode = keyCode;
+        	lastRacketKeyCode = keyCode;
             theRacket.move();
         	draw();
             return false;
+    	} else if (keyCode === 83) {
+    		// s
+    		serve();
     	}
     });
+    
+    var serve = function() {
+        theBall = ball({
+            ctx : ctx,
+            x : width / 2,
+            y : 20,
+            r : 10,
+            vector2d : vector2d(3 - Math.floor((Math.random() * 7)), 10),
+            color : 'white'
+        });
+        currentScore = 0;
+        currentScoreElement.html(currentScore);
+        var intervalId = setInterval(function() {
+        	var theVector2d;        
+        	//TODO Make criteria more precise!
+        	if (theBall.getY() + theBall.getR() >= theRacket.getY() && (theBall.getX() + theBall.getR() < theRacket.getX() || theBall.getX() - theBall.getR() > theRacket.getX() + theRacket.getW())) {
+        		// racket missed ball
+        		clearInterval(intervalId);
+        		if (currentScore >= highScore) {
+        			highScore = currentScore;
+        			highScoreElement.html(highScore);
+        		}
+        		theBall = null;
+        	} else {
+        		if (theBall.getY() + theBall.getR() >= theRacket.getY()) {
+        			// racket hits the ball
+                	theVector2d = theBall.getVector2d();
+                	theVector2d.add(vector2d(0, - 2 * theVector2d.getVy()));
+                	if (theBall.getX() < theRacket.getX() + theRacket.getW() / 3) {
+                		//theVector2d.rotate(- Math.PI * 6);
+                		theVector2d.add(vector2d(-1, 0));
+                	} else if (theBall.getX() > theRacket.getX() + theRacket.getW() * 2 / 3) {
+                		//theVector2d.rotate(Math.PI * 6);
+                		theVector2d.add(vector2d(1, 0));
+                	}
+                	currentScore++;
+                	currentScoreElement.html(currentScore);
+        		} else if (theBall.getY() - theBall.getR() <= 0) {
+            		// ball hits top wall
+                	theVector2d = theBall.getVector2d();
+                	theVector2d.add(vector2d(0, - 2 * theVector2d.getVy()));
+                } else if (theBall.getX() - theBall.getR() <= 0 || theBall.getX() + theBall.getR() >= width) {
+                	// ball hits left or right wall 
+                	theVector2d = theBall.getVector2d();
+                	theVector2d.add(vector2d(-2 * theVector2d.getVx(), 0));
+                }        
+                theBall.move();
+        	}
+        	draw();
+        }, 30);
+    }
 
-    setInterval(function() {
-    	var theVector2d;
-    	//TODO Make criteria more precise!
-    	if (theBall.getY() + theBall.getR() >= theRacket.getY() && (theBall.getX() < theRacket.getX() || theBall.getX() > theRacket.getX() + theRacket.getW())) {
-    		console.log("looser");
-    		theBall = ball({
-    	        ctx : ctx,
-    	        x : width / 2,
-    	        y : 20,
-    	        r : 10,
-    	        vector2d : vector2d(3 - Math.floor((Math.random() * 7)), 10),
-    	        color : 'white'
-    	    });
-    	}
-    	
-        if (theBall.getY() + theBall.getR() >= theRacket.getY() || theBall.getY() - theBall.getR() <= 0) {
-        	theVector2d = theBall.getVector2d();
-        	theVector2d.add(vector2d(0, - 2 * theVector2d.getVy()));
-        }
-        if (theBall.getX() + theBall.getR() >= width || theBall.getX() - theBall.getR() <= 0) {
-        	theVector2d = theBall.getVector2d();
-        	theVector2d.add(vector2d(-2 * theVector2d.getVx(), 0));
-        }        
-        theBall.move();
-        draw();
-    }, 30);
-
+    serve();
 
 });
